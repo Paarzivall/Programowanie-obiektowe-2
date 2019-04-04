68ccff65945fc758bfe33d825781889419a71807
Parzivall <mateusz.bugaj@interia.pl>
2019-04-04 12:13:38 +0200

    add

--- Lista zadań nr. 3/Zadanie numer 4/zadanie_nr_4.py
+++ Lista zadań nr. 3/Zadanie numer 4/zadanie_nr_4.py
@@ -2,23 +2,36 @@ def gen_time(start, stop, hop):
     czasStart = list(start)
     czasStop = list(stop)
     czasHop = list(hop)
-    print(type(czasStart))
-    """while czasStart[0]<=czasStop[0]and czasStart[1]<=czasStop[1]and czasStart[2]<=czasStop[2]:
-        tmp = czasStart[1]+czasHop[1]
-        tmp2 = czasStart[2]+czasHop[2]
-        if tmp>=60:
-            czasStart[1]+=tmp-60
-            czasStart[0]+=1
-        if tmp2>=60:
-            tmp2-=60
-            czasStart[2]+=tmp2
-            czasStart[1]+=1
 
-        yield czasStart"""
+    while True:
+        tupla = (czasStart[0], czasStart[1], czasStart[2])
+        yield tupla
+        tmp_h = czasStart[0] + czasHop[0]
+        tmp_min = czasStart[1] + czasHop[1]
+        tmp_sec = czasStart[2] + czasHop[2]
+        if tmp_h >= 24:
+            czasStart[0] = 0
+        else:
+            czasStart[0] += czasHop[0]
 
+        if tmp_sec >= 60:
+            czasStart[1] += 1
+            czasStart[2] = tmp_sec - 60
+        else:
+            czasStart[2] += czasHop[2]
+
+        if tmp_min >= 60:
+            czasStart[0] += 1
+            czasStart[1] = tmp_min - 60
+        else:
+            czasStart[1] += czasHop[1]
+
+        if czasStart[0] >= czasStop[0]:
+            if czasStart[1] >= czasStop[1]:
+                break
 
 
 if __name__ == '__main__':
 
-    for time in gen_time((8,10,0),(10,50,15),(0,15,12)):
+    for time in gen_time((8, 10, 0), (10, 50, 15), (0, 15, 12)):
         print(time)
