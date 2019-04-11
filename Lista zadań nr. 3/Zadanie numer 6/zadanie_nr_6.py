922d582495c7ba32b5e648684b17c387caf4c19a
Parzivall <mateusz.bugaj@interia.pl>
2019-04-11 12:25:28 +0200

    add

--- Lista zadań nr. 3/Zadanie numer 6/zadanie_nr_6.py
+++ Lista zadań nr. 3/Zadanie numer 6/zadanie_nr_6.py
@@ -1,4 +1,4 @@
-def flatten(data):
+"""def flatten(data):
     for x in data:
         if isinstance(x, list):
             yield from flatten(list(x))
@@ -11,5 +11,21 @@ def flatten(data):
 if __name__ == '__main__':
     all = []
     for x in flatten(([1, 'kot'], 3, (4, 5, [7, 8, 9]))):
+        all.append(x)
+    print(all)"""
+
+import collections.abc
+
+def flatten(data):
+    for x in data:
+        if isinstance(x, collections.abc.Iterable) and not isinstance(x, str):
+            yield from flatten(x)
+        else:
+            yield x
+
+
+if __name__ == '__main__':
+    all = []
+    for x in flatten(([1, 'kot'], 3, (4, 5, [7, 8, 9, {2, 3, 4, (9, 8)}]))):
         all.append(x)
     print(all)
